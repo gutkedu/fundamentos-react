39929fc1fd97e5feaf021f579430423067dea163
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2025-12-11 23:18:13 -0300

    feat: implement Sidebar component with styling and profile details

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,21 @@
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
-  return <aside className={styles.sidebar}>Sidebar content</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Developer working on a laptop"
+      ></img>
+      <div className={styles.profile}>
+        <strong>Jane Doe</strong>
+        <span>Full Stack Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
 }
